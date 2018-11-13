dec0555e55e709a919256cef7a09e043a6097c8d
John Lynch <johnlynch.32768@gmail.com>
2018-11-13 02:28:28 +0000

    Got routes & users basically sorted

--- run.py
+++ run.py
@@ -1,16 +1,18 @@
-from flask import Flask, redirect, render_template, request, url_for # flash
+from flask import Flask, redirect, render_template, request, url_for, flash
 from operator import add, sub, mul, truediv
 import os
 # from datetime import datetime
 # import json
 
 app = Flask(__name__)
-
+app.secret_key = '12676506002A2822HOD940149670../"^$CCC320537618446744074370f95g51616'
 ops = {'+': add, '-': sub, 'x': mul, '/': truediv}
 # initial test data...
 term1 = 6
 term2 = 28
 op = '+'
+users = []
+
 # --------------------
 
 class User:
@@ -22,7 +24,7 @@ class User:
         self.qs_correct = 0
 
 # keep a list of known users
-users = []
+
 
 class Q:
     """ Q, or Question class represents an arithmetical problem """
@@ -32,32 +34,35 @@ class Q:
         self.y = y
         self.op = op
 
-@app.route("/", methods = ["GET", "POST"])
-def index():
+@app.route("/")
+def index():    
+    return render_template("index.html")
+
+
+
+@app.route("/problems", methods = ["GET", "POST"]) 
+def ask_qs():
     if request.method == "POST":
         uname = request.form["uname"]
-        userid = next((i for i, user in enumerate(users) if user.name == u), -1)
-        if userid = -1:
+        userid = next((i for i, user in enumerate(users) if user.name == uname), -1)
+        if userid == -1:
             userid = len(users)
             user = User(uname)
             users.append(user)
         else:
             user = users[userid]
+        # Here calculate values and operation; for now they're fixed;
+        return render_template("problems.html", uname = user.name, x = term1, op = op, y = term2)
         
 
-        return render_template("ask_q.html", uname = u, x = term1, op = op, y = term2)
-
-    return render_template("index.html")
-
-@app.route("/ask_q", methods = ["GET", "POST"]) 
-def ask_q():
+@app.route("/mark", methods = ["GET", "POST"])
+def mark():
     if request.method == "POST":
-        u = request.form["user_answer"]
-        return render_template("mark.html", uname = "Anon", user_answer = u)  
-    return render_template("ask_q.html", uname = "Anon", x = term1, op = op, y = term2)
+        ua = request.form["user_answer"]
+        return f'{ua} is {"" if ua == "34" else "in"}correct'
+         
 
-@app.route("/mark", methods = ["GET"])
-def mark(uname, uanswer):
-    return render_template("mark.html", uname = uname, user_answer = uanswer)
+
+
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT', 5000)), debug=True)
